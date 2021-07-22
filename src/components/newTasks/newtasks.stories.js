61bb8c148abf9ecdf87b5f94e22cd19ae59e6750
ChristianSeelemann <cseelemann@gmx.de>
2021-07-22 15:45:55 +0200

    Add new task component - basic structure.

---
+++ src/components/newTasks/newtasks.stories.js
@@ -0,0 +1,9 @@
+import "./newTasks.css";
+import newTasks from "./newTasks.html";
+
+export default {
+  title: "Components/NewTask",
+  parameters: { layout: "fullscreen" },
+};
+
+export const newTaskComponent = () => newTasks;
